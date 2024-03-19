af68c381095244da489758e9c731dfc0d223d5c2
geogeolo <163122629+geogeolo@users.noreply.github.com>
2024-03-20 00:50:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 required_version = version.parse("1.1.1")
 current_version = version.parse(openai.__version__)
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 if current_version < required_version:
     raise ValueError(
